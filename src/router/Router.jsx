1d3b220212bf352ff98b0652953b22da24c3747b
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-02-01 14:22:10 +0100

    feat: :white_check_mark: Add Navbar in all routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../views/home/Home";
+import Navbar from "../components/navbar/Navbar";
 
 const Router = () => {
    return (
       <BrowserRouter>
+         <Navbar />
          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/home" element={<Home/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
          </Routes>
       </BrowserRouter>
    );
