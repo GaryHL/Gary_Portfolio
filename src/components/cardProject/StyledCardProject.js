f89ac0b2c0e3c1224c3ebfa8af3b6435d11e55b5
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-02-01 23:51:29 +0100

    feat: :white_check_mark: Create Card Project

---
+++ src/components/cardProject/StyledCardProject.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+
+export const StyledContainerCard = styled(motion.div)`
+   border: 1px solid #333;
+   min-width: 250px;
+   max-width: 300px;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   &:hover {
+      > img {
+         animation: flash 0.3s;
+         filter: brightness(var(--value, 5));
+         --value: 0.5;
+         @keyframes flash {
+            0% {
+               filter: brightness(var(--value, 5));
+               --value: 2;
+            }
+            100% {
+               filter: brightness(var(--value, 5));
+               --value: 0.5;
+            }
+         }  
+      }
+   }
+`;
+
+export const StyledImgCard = styled(motion.img)`
+   width: 100%;
+   aspect-ratio: 16/9;
+   object-fit: cover;
+   cursor:pointer;
+`;
+
+export const StyledDetailsCard = styled(motion.span)`
+   padding: 1rem;
+   min-height: 3rem;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   gap: 0.5rem;
+`;
+
+export const StyledCardButtons = styled(motion.div)`
+padding:0.5rem;
+width:100%;
+display:flex;
+justify-content:center;
+gap:1rem;
+
+`
+
+export const ButtonCard = styled(Link)`
+width:100px;
+background-color:#e9e9e9;
+border:none;
+outline:none;
+border:1px solid  #333;
+cursor:pointer;
+padding:0.5rem;
+text-decoration:none;
+color:black;
+display: flex;
+align-items:center;
+justify-content:center;
+&:hover{
+    background: black;
+    color:#e9e9e9;
+    transition:ease-in-out 0.3s;
+}
+`
