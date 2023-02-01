f08571db82835863c5414db9538b926a83594bbc
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-02-01 14:17:21 +0100

    feat: :white_check_mark: Create scroll efect
    Create scroll effect, and use observer api to animate

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,95 +1,214 @@
 import React, { useState, useRef, useEffect } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import SectionHome from "../../components/sectionHome/SectionHome";
+import ReactFullpage from "@fullpage/react-fullpage";
 
 const Home = () => {
-   const divRef = useRef(null);
-
-   function scrollToHome() {
-      const element = document.querySelector("#home");
-      const y = element.getBoundingClientRect().top + window.scrollY;
-      window.scrollTo({ top: y, behavior: "smooth" });
-   }
-
-   function scrollToProjects() {
-      const element = document.querySelector("#projects");
-      const y = element.getBoundingClientRect().top + window.scrollY;
-      window.scrollTo({ top: y, behavior: "smooth" });
-   }
-
-   function scrollToAbout() {
-      const element = document.querySelector("#about");
-      const y = element.getBoundingClientRect().top + window.scrollY;
-      window.scrollTo({ top: y, behavior: "smooth" });
-   }
-
-   function scrollToContact() {
-      const element = document.querySelector("#contact");
-      const y = element.getBoundingClientRect().top + window.scrollY;
-      window.scrollTo({ top: y, behavior: "smooth" });
-   }
-
+   //estados iniciales de cada vista
    const [section1, setSection1] = useState(false);
    const [section2, setSection2] = useState(false);
    const [section3, setSection3] = useState(false);
    const [section4, setSection4] = useState(false);
 
-   const [sectionActive, setSectionActive] = useState("#home");
+   //función que obtiene el id y te lleva ahí
+   const [sectionActive, setSectionActive] = useState("#projects");
 
+   //estado constante, que se activa cada vez que el useState cambia
    useEffect(() => {
       switch (sectionActive) {
          case "#home":
-            scrollToHome();
             setSection1(true);
             setSection2(false);
             setSection3(false);
             setSection4(false);
-            console.log("estás en home");
             break;
          case "#projects":
-            scrollToProjects();
             setSection1(false);
             setSection2(true);
             setSection3(false);
             setSection4(false);
-            console.log("estás en projects");
             break;
          case "#about":
-            scrollToAbout();
             setSection1(false);
             setSection2(false);
             setSection3(true);
             setSection4(false);
-            console.log("estás en About");
             break;
          case "#contact":
-            scrollToContact();
             setSection1(false);
             setSection2(false);
             setSection3(false);
             setSection4(true);
-            console.log("estás en Contat");
             break;
          default:
-            scrollToHome();
             setSection1(true);
             setSection2(false);
             setSection3(false);
             setSection4(false);
-            console.log("estás en home");
             break;
       }
    }, [sectionActive]);
 
+   //estado que contiene el div que está en pantalla
+   const [inScreen, setInScreen] = useState(null);
+
+   const divRef1 = useRef(null);
+   const divRef2 = useRef(null);
+   const divRef3 = useRef(null);
+   const divRef4 = useRef(null);
+
+   useEffect(() => {
+      const observer = new IntersectionObserver((entries) => {
+         entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+               setInScreen(entry.target.id);
+               if (entry.target.id !== sectionActive) {
+                  setSectionActive(entry.target.id);
+               }
+            }
+         });
+      });
+      observer.observe(divRef1.current);
+      observer.observe(divRef2.current);
+      observer.observe(divRef3.current);
+      observer.observe(divRef4.current);
+   }, [sectionActive]);
+
+   //data de las secciones
+
+   const dataHome = {
+      title: "Hola, soy Gary",
+      subtitle: "This is a subtitle",
+      ctaText: "click me",
+      cta: () => {
+         alert("You clicked");
+      },
+      picture:
+         "https://imgs.search.brave.com/tkMjyNJx9iq01utpQKcUnrnIrqOA5EosNJ16_wqCTYU/rs:fit:1024:410:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzEzLzFk/L2ZjLzEzMWRmYzg5/ZjMwYTllODRlOGJm/Njc2YTc1MzY1MTgw/LmpwZw",
+      pictureName: "Image name",
+   };
+
+   const dataProjects = {
+      title: "Proyectos",
+      subtitle: "This is a subtitle",
+      ctaText: "click me",
+      cta: () => {
+         alert("You clicked");
+      },
+      picture:
+         "https://imgs.search.brave.com/tkMjyNJx9iq01utpQKcUnrnIrqOA5EosNJ16_wqCTYU/rs:fit:1024:410:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzEzLzFk/L2ZjLzEzMWRmYzg5/ZjMwYTllODRlOGJm/Njc2YTc1MzY1MTgw/LmpwZw",
+      pictureName: "Image name",
+   };
+
+   const dataAbout = {
+      title: "Sobre mí",
+      subtitle: "This is a subtitle",
+      ctaText: "click me",
+      cta: () => {
+         alert("You clicked");
+      },
+      picture:
+         "https://imgs.search.brave.com/tkMjyNJx9iq01utpQKcUnrnIrqOA5EosNJ16_wqCTYU/rs:fit:1024:410:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzEzLzFk/L2ZjLzEzMWRmYzg5/ZjMwYTllODRlOGJm/Njc2YTc1MzY1MTgw/LmpwZw",
+      pictureName: "Image name",
+   };
+
+   const dataContact = {
+      title: "Contacto",
+      subtitle: "This is a subtitle",
+      ctaText: "click me",
+      cta: () => {
+         alert("You clicked");
+      },
+      picture:
+         "https://imgs.search.brave.com/tkMjyNJx9iq01utpQKcUnrnIrqOA5EosNJ16_wqCTYU/rs:fit:1024:410:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzEzLzFk/L2ZjLzEzMWRmYzg5/ZjMwYTllODRlOGJm/Njc2YTc1MzY1MTgw/LmpwZw",
+      pictureName: "Image name",
+   };
+
    return (
-      <>
-         <Navbar />
-         <SectionHome active={section1} idSection="home" />
-         <SectionHome active={section2} idSection="projects" />
-         <SectionHome active={section3} idSection="about" />
-         <SectionHome active={section4} idSection="contact" />
-      </>
+      <ReactFullpage
+         // licenseKey={"YOUR_KEY_HERE"}
+         scrollingSpeed={1000}
+         render={({ state, fullpageApi }) => {
+            return (
+               <>
+                  <Navbar />
+                  <ReactFullpage.Wrapper>
+                     <div className="section">
+                        <div id="home">
+                           <SectionHome
+                              active={section1}
+                              dataSection={dataHome}
+                           >
+                              <div
+                                 style={{
+                                    position: "absolute",
+                                    height: "0.1%",
+                                 }}
+                                 ref={divRef1}
+                                 id="#home"
+                              ></div>
+                           </SectionHome>
+                        </div>
+                     </div>
+                     <div className="section">
+                        <div id="projects">
+                           <SectionHome
+                              active={section2}
+                              dataSection={dataProjects}
+                              withButton={true}
+                           >
+                              <div
+                                 style={{
+                                    position: "absolute",
+                                    height: "0.1%",
+                                 }}
+                                 ref={divRef2}
+                                 id="#projects"
+                              ></div>
+                           </SectionHome>
+                        </div>
+                     </div>
+                     <div className="section">
+                        <div id="about">
+                           <SectionHome
+                              active={section3}
+                              dataSection={dataAbout}
+                              withButton={true}
+                           >
+                              <div
+                                 style={{
+                                    position: "absolute",
+                                    height: "0.1%",
+                                 }}
+                                 ref={divRef3}
+                                 id="#about"
+                              ></div>
+                           </SectionHome>
+                        </div>
+                     </div>
+                     <div className="section">
+                        <div id="contact">
+                           <SectionHome
+                              active={section4}
+                              dataSection={dataContact}
+                              withButton={true}
+                           >
+                              <div
+                                 style={{
+                                    position: "absolute",
+                                    height: "0.1%",
+                                 }}
+                                 ref={divRef4}
+                                 id="#contact"
+                              ></div>
+                           </SectionHome>
+                        </div>
+                     </div>
+                  </ReactFullpage.Wrapper>
+               </>
+            );
+         }}
+      />
    );
 };
-
 export default Home;
