247c27919301ec2de2d7ce02416f3eb944d0b2fc
Gary HL <garyhl321@gmail.com>
2023-05-02 17:59:36 +0200

    change to hash router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, HashRouter } from "react-router-dom";
 import Home from "../views/home/Home";
 import Navbar from "../components/navbar/Navbar";
 import Projects from "../views/projects/Projects";
@@ -10,19 +10,21 @@ const Router = () => {
    const [formActive, setFormActive] = useState()
 
    return (
-      <BrowserRouter>
-         <Navbar />
-         <Routes>
-            <Route path="/" element={<Home setFormActive={setFormActive}/>} />
-            <Route path="/:page" element={<Home setFormActive={setFormActive}/>} />
-            <Route path="/home/:page" element={<Home setFormActive={setFormActive}/>} />
-            <Route path="/home" element={<Home setFormActive={setFormActive}/>} />
-            <Route path="/projects" element={<Projects/>} />
-            <Route path="/about" element={<AboutMe/>} />
-            <Route path="/abouta" element={<ContactForm/>} />
-         </Routes>
-         <ContactForm formActive={formActive} setFormActive={setFormActive}></ContactForm>
-      </BrowserRouter>
+      <HashRouter>
+         <>
+            <Navbar />
+            <Routes>
+               <Route path="/" element={<Home setFormActive={setFormActive} />} />
+               <Route path="/:page" element={<Home setFormActive={setFormActive} />} />
+               <Route path="/home/:page" element={<Home setFormActive={setFormActive} />} />
+               <Route path="/home" element={<Home setFormActive={setFormActive} />} />
+               <Route path="/projects" element={<Projects />} />
+               <Route path="/about" element={<AboutMe />} />
+               <Route path="/abouta" element={<ContactForm />} />
+            </Routes>
+            <ContactForm formActive={formActive} setFormActive={setFormActive}></ContactForm>
+         </>
+      </HashRouter>
    );
 };
 
